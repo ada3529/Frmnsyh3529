b21129d1ddefafd6fe750b15bc952492b118275e
ada3529 <98525897+ada3529@users.noreply.github.com>
2022-06-25 12:49:33 +0700

    Update and rename connect/config.js to Menghubung/config.js

--- Menghubung/config.js
+++ Menghubung/config.js
@@ -7,7 +7,7 @@ Other*/
 global.thum = fs.readFileSync("./worker/media/image/thumb.jpg")
 global.log0 = fs.readFileSync("./worker/media/image/thumb.jpg")
 global.err4r = fs.readFileSync("./worker/media/image/thumb.jpg")
-global.owner = ['6289501060783','6289501060783','6289501060783']
+global.owner = ['6281779012824','6283188622570','6289501060783']
 global.footer = "Deffbotz ~ Deff"
 global.fake = "Deffbotz ~ Deff"
 global.namebot = "DEFFBOTZ - DEFF"
